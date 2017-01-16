25d9dae7f9430b51e9c9b84de1d66d14f6855724
wlovell2 <300833478@cencol.ca>
2017-01-16 18:50:16 -0500

    working on Patient class

--- src/exercise1/Patient.java
+++ src/exercise1/Patient.java
@@ -1,5 +1,30 @@
 package exercise1;
 
 public class Patient {
-
+	int patient_id;
+	String firstname;
+	String lastname;
+	int age;
+	
+	public int getPatient_id()
+	{
+		return this.patient_id;
+	}
+	public void setPatient_id(int value)
+	{
+		this.patient_id=value;
+	}
+	public String getFirstname(){
+	return this.firstname;
+}
+	public void setFirstName(String value){
+		this.firstname=value;
+	}
+	public String getLastname(){
+		return this.lastname;
+	}
+	
+	public int getAge(){
+		return this.age;
+	}
 }
